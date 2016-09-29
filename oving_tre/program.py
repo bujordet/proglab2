2dd0966425d7b1a9834fa0049a90c66994319ea3
Morten Bujordet <mobujordet@Mortens-MacBook-Pro-2.local>
2016-09-29 20:30:08 +0200

    lagt inn hele alfabetet og endret antall tegn.

--- oving_tre/program.py
+++ oving_tre/program.py
@@ -3,9 +3,15 @@ import Cipher
 
 
 class main():
-    test = Cipher.Multiplicative(3)
+    test = Cipher.Caesar(3)
+    test2 = Cipher.Multiplicative(3)
     encode = test.encode("HAGE")
     decode = test.decode(encode)
+    encode2 = test2.encode("BADE")
+    decode2 = test2.decode(encode2)
     print(encode)
     print(decode)
+    print(encode2)
+    print(decode2)
     print(test.verify("HAGE"))
+    print(test2.verify("BADE"))
