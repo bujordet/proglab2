04e435299b6a8dccd5ec68ab09f21a0c42931421
bujordet <mobujordet@Mortens-iMac.local>
2016-09-29 23:24:47 +0200

    Fikk fikset feil i Multiplekser. Fikk også ferdigstilt Affine og Unbrakable

--- oving_tre/Cipher.py
+++ oving_tre/Cipher.py
@@ -70,6 +70,8 @@ class Multiplicative(Cipher):
         self.key = key
 
     def encode(self, klarTekst):
+        #if (self.check_key() is False):
+            #print("Key is not 1to1")
         kodet_tekst = ""
         for symbol in klarTekst:
             orginal = self.alfabetet.index(symbol)
@@ -79,12 +81,74 @@ class Multiplicative(Cipher):
             kodet_tekst += self.alfabetet[tall]
         return kodet_tekst
 
-    def decode(self, klarTekst):
-        kodet_tekst = ""
-        for symbol in klarTekst:
+    def decode(self, kryptTekst):
+        #print(self.tegn)
+        #print(self.key)
+        klar_tekst = ""
+        for symbol in kryptTekst:
             orginal = self.alfabetet.index(symbol)
-            tall = ((orginal * self.key)%self.tegn)*crypto_utils.modular_inverse(self.key, self.tegn)%self.tegn
-            #if (tall >= self.tegn):
-            #    tall = tall%self.tegn
-            kodet_tekst += self.alfabetet[tall]
+            m = crypto_utils.modular_inverse(self.key, self.tegn)
+            #print(m)
+            ny_key = (orginal*m)%self.tegn
+            klar_tekst += self.alfabetet[ny_key]
+        return klar_tekst
+
+    def check_key(self):
+        m = crypto_utils.modular_inverse(self.key, self.tegn)
+        return (self.key*m == 1%self.tegn)
+
+class Affine(Cipher):
+    def __init__(self, key):
+        super().__init__()
+        self.keyone = key[0]
+        self.keytwo = key[1]
+
+    def encode(self, klar_tekst):
+        kodet_tekst = Multiplicative(self.keyone).encode(klar_tekst)
+        #print(kodet_tekst)
+        resultat = Caesar(self.keytwo).encode(kodet_tekst)
+        return resultat
+
+    def decode(self, kodet_tekst):
+        steg_en = Caesar(self.tegn - self.keytwo).encode(kodet_tekst)
+        dekodet_tekst = Multiplicative(self.keyone).decode(steg_en)
+        return dekodet_tekst
+
+class Unbrakable(Cipher):
+    def __init__(self, keyword):
+        super().__init__()
+        self.keyword = keyword
+
+    def encode(self, klar_tekst):
+        kodet_tekst = ""
+        count = 0
+        for letter in klar_tekst:
+            verdi_tekst = self.alfabetet.index(letter)
+            verdi_kodeord = self.alfabetet.index(self.keyword[count])
+            totalverdi = verdi_kodeord + verdi_tekst
+            if (totalverdi >= self.tegn):
+                totalverdi = totalverdi%self.tegn
+            kodet_tekst += self.alfabetet[totalverdi]
+            if (count == len(self.keyword)-1):
+                count = 0
+
         return kodet_tekst
+
+    def decode(self, kodet_tekst):
+        dekodet_tekst = ""
+        lureord = []
+        for letter in self.keyword:
+            verdi_kodeord = self.alfabetet.index(letter)
+            verdi_lureord = (self.tegn - verdi_kodeord)%self.tegn
+            lureord.append(verdi_lureord)
+        x = 0
+        for character in kodet_tekst:
+            verdi_tekst = self.alfabetet.index(character)
+            tilbake_verdi = verdi_tekst + lureord[x]
+            if (tilbake_verdi >= self.tegn):
+                tilbake_verdi = tilbake_verdi%self.tegn
+            dekodet_tekst += self.alfabetet[tilbake_verdi]
+            if (x == len(lureord)-1):
+                x = 0
+
+        return dekodet_tekst
--- oving_tre/program.py
+++ oving_tre/program.py
@@ -1,17 +1,28 @@
-
 import Cipher
 
 
 class main():
     test = Cipher.Caesar(3)
     test2 = Cipher.Multiplicative(3)
+    test3 = Cipher.Affine((3,2))
+    test4 = Cipher.Unbrakable("RUKE")
+
     encode = test.encode("HAGE")
     decode = test.decode(encode)
-    encode2 = test2.encode("BADE")
+    encode2 = test2.encode("KUKE")
     decode2 = test2.decode(encode2)
-    print(encode)
-    print(decode)
-    print(encode2)
-    print(decode2)
+    encode3 = test3.encode("RUKE")
+    decode3 = test3.decode(encode3)
+    encode4 = test4.encode("PIZZATRYNE")
+    decode4 = test4.decode(encode4)
+
+
+    print(encode, decode)
+    print(encode2,decode2)
+    print(encode3 , decode3)
+    print(encode4, decode4)
+
     print(test.verify("HAGE"))
     print(test2.verify("BADE"))
+    print(test3.verify("RUKE"))
+    print(test4.verify("PIZZATRYNE"))
